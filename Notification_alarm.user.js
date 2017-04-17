1208dd28e5ded649d13d997cb5410773b933101d
DLRG-Dominik <dlehnen1@web.de>
2017-04-17 18:30:49 +0200

    Update Notification_alarm.user.js

--- Notification_alarm.user.js
+++ Notification_alarm.user.js
@@ -5,92 +5,95 @@
 // @author       DLRG-Dominik
 // @include      *://www.leitstellenspiel.de/
 // @exclude      *://www.leitstellenspiel.de/mission*
-// @updateURL    https://dl.dropboxusercontent.com/u/49293607/Meine_scripte/Scriptdatenbank/SL/Leitstellenspiel/Chat.user.js
+// @updateURL    https://github.com/DLRG-Dominik/LSSNotifity-Alarm/blob/master/Notification_alarm.user.js
 // @grant        none
 // ==/UserScript==
-function notifyMe(username,message) {
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
+function notifyMe(username,message,type="init",fms="2") {
 
-  // Let's check whether notification permissions have alredy been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification('Leitstellenspiel.de - Chat - Nachricht von '+username, {
-                body: message
-            });
-  }
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
 
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification("Hi there!");
-      }
-    });
-  }
 
-  // At last, if the user has denied notifications, and you 
-  // want to be respectful there is no need to bother them any more.
+    else if (Notification.permission === "granted") {
+
+        if(type =="init")
+        {
+            var notification = new Notification(username, {
+                body: message,
+                icon: "https://www.leitstellenspiel.de/images/logo-header.png"
+            });  
+        }
+        else if(type =="Chat")
+        {
+            var notification = new Notification('Chatnachricht von '+username, {
+                body: message,
+                icon: "https://raw.githubusercontent.com/DLRG-Dominik/LSSNotifity-Alarm/master/134895.png"
+            });  
+        }
+        else if(type =="Status")
+        {
+            var notification = new Notification(username, {
+                body: message,
+                icon: "https://raw.githubusercontent.com/DLRG-Dominik/LSSNotifity-Alarm/master/Status_"+fms+".png" 
+            });  
+        }
+
+    }
+
+
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+
+            if (permission === "granted") {
+                var notification = new Notification("Benachrichtungen aktiviert!");
+            }
+        });
+    }
+
+
 }
-
+notifyMe("Initalisierung...","Notification-Alarm wird initalisiert, Bitte warten...","init");
 (function(){
-   
-    
-    var $mainDiv = $('<div id="chatNote" class="panel panel-default"><div class="panel-heading">Chat</div></div>');
-    $mainDiv.css({
-        'position': 'fixed',
-        'width': '250px',
-        'z-index': '99999',
-        'margin-left': '-255px',
-        'top': '20%',
-        'left': '100%',
-        'display':'none',
-        'cursor':'pointer'
-    });
-    $mainDiv.click(function(){
-        $(this).hide('slow');
-    });
-    var $contentDiv = $('<div class="panel-body" style="background-color: white;"></div>');
-    var $ul = $('<ul id="chatNoteUl"></ul>');
-    $ul.css({
-        'list-style': 'none',
-        'margin-left': '0',
-        'padding-left':' 20px'
-    });
-    $ul.appendTo($contentDiv);
-    $contentDiv.appendTo($mainDiv);
-    $mainDiv.appendTo($('body'));
+
     var allianceChatNotifcation = true;
-    $('<a href="#" class="btn btn-success btn-xs pull-right" title="Chatnotification aus/ein schalten">Chatnotification</a>').appendTo($('#chat_outer .panel-heading'))
-            .click(function(e){
-                allianceChatNotifcation = !allianceChatNotifcation;
-                $(this).hasClass("btn-success") ? $(this).removeClass("btn-success").addClass("btn-danger"): $(this).addClass("btn-success").removeClass("btn-danger");
-                return false;
+    var allianceS5Notifcation = true;
+    var allianceStatusNotifcation = false;
+    $('<a href="#" class="btn btn-success btn-xs pull-right" title="Notification-Alarm aus/ein schalten">N-A: Chat</a>').appendTo($('#chat_outer .panel-heading'))
+        .click(function(e){
+        allianceChatNotifcation = !allianceChatNotifcation;
+        $(this).hasClass("btn-success") ? $(this).removeClass("btn-success").addClass("btn-danger"): $(this).addClass("btn-success").removeClass("btn-danger");
+        return false;
+    });
+    $('<a href="#" class="btn btn-success btn-xs pull-right" title="Notification-Alarm aus/ein schalten">N-A: Status 5</a>').appendTo($('#chat_outer .panel-heading'))
+        .click(function(e){
+        allianceS5Notifcation = !allianceS5Notifcation;
+        $(this).hasClass("btn-success") ? $(this).removeClass("btn-success").addClass("btn-danger"): $(this).addClass("btn-success").removeClass("btn-danger");
+        return false;
+    });
+    $('<a href="#" class="btn btn-danger btn-xs pull-right" title="Notification-Alarm aus/ein schalten">N-A: Status</a>').appendTo($('#chat_outer .panel-heading'))
+        .click(function(e){
+        allianceStatusNotifcation = !allianceStatusNotifcation;
+        $(this).hasClass("btn-success") ? $(this).removeClass("btn-success").addClass("btn-danger"): $(this).addClass("btn-success").removeClass("btn-danger");
+        return false;
     });
     var allianceChatBuffer = allianceChat;
-    var MainDivTimer;
-    function hideMainDiv(){
-        clearTimeout(MainDivTimer);
-        MainDivTimer = setTimeout(function(){
-            $mainDiv.hide('slow');
-        },6000); 
-    }
+    var radioMessageBuffer = radioMessage;
     allianceChat = function(t){
         allianceChatBuffer(t);
         if(user_id !== t.user_id && allianceChatNotifcation){
-            notifyMe(t.username,t.message);
-            console.log("Notification-ALARM");
-            var e = "<li><span class='mission_chat_message_username'>[" + t.date + "] <a href='/profile/" + t.user_id + "' class='lightbox-open'>" + t.username + ":</a></span>";
-            t.mission_id && (e = e + "<a href='/missions/" + t.mission_id + "' class='lightbox-open'><span class='glyphicon glyphicon-bell'></span></a> ");
-            e = e + " " + t.message + "</li>";
-            
-            $(e).appendTo($ul).delay(6000).hide('slow',function(){$(this).remove();});
-            $mainDiv.show('slow');
-            
-            hideMainDiv();
+
+            notifyMe(t.username,t.message,"Chat");
+        }
+    };
+    radioMessage = function(t){
+        radioMessageBuffer(t);
+        if(t.fms_real == 5 && allianceS5Notifcation && alliance_ignore_fms){
+            console.log(alliance_ignore_fms);
+            notifyMe(t.caption,t.fms_text,"Status",t.fms_real);
+        }
+        else if(t.fms_real != 5 && allianceStatusNotifcation){
+            notifyMe(t.caption,t.fms_text,"Status",t.fms_real);
         }
     };
 })();
